d5ff1c315be59b8b0820e4e09b5af4554a740f0a
Tintin <tanner@schenck.xyz>
2020-10-21 00:14:20 -0400

    connection created with SQL workbench

--- config/connection.js
+++ config/connection.js
@@ -6,10 +6,12 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "C!ive.1804",
-    database: "burgers_db"
+    
+    host: "vrk7xcrab1wsx4r1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
+    port: 3306,
+    user: "eunr5tk4v1y7c5jt	",
+    password: "gaplih0h9luuu3c2",
+    database: "y5dfx6zon3frlaaz	"
   });
 };
 
